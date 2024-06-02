4f169f053ccef81588e14834bc68016de1899507
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-02 14:57:05 +0200

    Add  bottom nav to home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_app/const.dart';
+import 'package:coffee_app/navigators/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,6 +9,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      bottomNavigationBar: MyBottomNavBar(),
     );
   }
 }

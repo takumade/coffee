b615a707884b6acba2aa75cb1eaf7ad673e79f8e
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-02 16:02:27 +0200

    Add more widgets

--- lib/components/coffee_tile.dart
+++ lib/components/coffee_tile.dart
@@ -5,17 +5,22 @@ import 'package:flutter/material.dart';
 
 class CoffeeTile extends StatelessWidget {
   final Coffee coffee;
-  const CoffeeTile({super.key, required this.coffee});
+  void Function()? onPressed;
+  CoffeeTile({super.key, required this.coffee, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Colors.grey[200]),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(12)),
       margin: EdgeInsets.only(bottom: 10),
+      padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
       child: ListTile(
         title: Text(coffee.name),
         subtitle: Text(coffee.price),
         leading: Image.asset(coffee.imagePath),
+        trailing: IconButton(icon: Icon(Icons.add), onPressed: onPressed,),
       ),
     );
   }
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,3 +1,5 @@
+import 'package:coffee_app/components/coffee_tile.dart';
+import 'package:coffee_app/models/coffee.dart';
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget {
@@ -10,8 +12,31 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("SHOP"),
-    );
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Column(
+        children: [
+          Text("How would you like your coffee?",
+              style: TextStyle(fontSize: 20)),
+          SizedBox(
+            height: 25,
+          ),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    Coffee coffee = Coffee(
+                        name: "Espresso",
+                        price: "3.50",
+                        imagePath: "lib/images/espresso.png");
+
+                    return CoffeeTile(
+                      coffee: coffee,
+                      onPressed: (){},);
+                  }))
+        ],
+      ),
+    ));
   }
 }

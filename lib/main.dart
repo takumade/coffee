e4268030c4e15fca3487d98090405a125c66abe6
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-02 19:36:16 +0200

    Add intro page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_app/pages/home_page.dart';
+import 'package:coffee_app/pages/into_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: IntroPage(),
     );
   }
 }

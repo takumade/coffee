c4da27daf925c64a4c905e520e0a49645f22905f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-02 18:12:10 +0200

    Route to coffee details page

--- lib/components/coffee_tile.dart
+++ lib/components/coffee_tile.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:coffee_app/models/coffee.dart';
+import 'package:coffee_app/pages/coffee_detail_page.dart';
 import 'package:flutter/material.dart';
 
 class CoffeeTile extends StatelessWidget {
@@ -10,17 +11,25 @@ class CoffeeTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(12)),
-      margin: EdgeInsets.only(bottom: 10),
-      padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
-      child: ListTile(
-        title: Text(coffee.name),
-        subtitle: Text(coffee.price),
-        leading: Image.asset(coffee.imagePath),
-        trailing: IconButton(icon: Icon(Icons.add), onPressed: onPressed,),
+    return GestureDetector(
+
+      onTap: (){
+        Navigator.push(
+          context, MaterialPageRoute(builder: (context) => CoffeeDetailPage(coffee: coffee))
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(12)),
+        margin: EdgeInsets.only(bottom: 10),
+        padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
+        child: ListTile(
+          title: Text(coffee.name),
+          subtitle: Text(coffee.price),
+          leading: Image.asset(coffee.imagePath),
+          trailing: IconButton(icon: Icon(Icons.add), onPressed: onPressed,),
+        ),
       ),
     );
   }

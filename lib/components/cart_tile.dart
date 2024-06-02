5b7bd9626e3b17f794291df66259e3e1decf731d
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-02 19:07:16 +0200

    Change text stength

--- lib/components/cart_tile.dart
+++ lib/components/cart_tile.dart
@@ -17,10 +17,10 @@ class CartTile extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 10),
       padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
       child: ListTile(
-        title: Text(coffee.name),
-        subtitle: Text(coffee.price),
+        title: Text(coffee.name, style: TextStyle(fontWeight: FontWeight.bold),),
+        subtitle: Text("\$ ${coffee.price}"),
         leading: Image.asset(coffee.imagePath),
-        trailing: IconButton(icon: Icon(Icons.add), onPressed: onPressed,),
+        trailing: IconButton(icon: Icon(Icons.delete, color: Colors.brown,), onPressed: onPressed,),
       ),
     );
   }

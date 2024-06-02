e35b8b81384a1eb5671a23aef5407d1a961c5c7f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-02 15:08:41 +0200

    Add ontab change function

--- lib/navigators/bottom_nav_bar.dart
+++ lib/navigators/bottom_nav_bar.dart
@@ -4,13 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
-  const MyBottomNavBar({super.key});
+
+  void Function(int)? onTabChange;
+  MyBottomNavBar({super.key, required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(25),
       child: GNav(
+        onTabChange: (value) => onTabChange!(value) ,
         color: Colors.grey[400],
         mainAxisAlignment: MainAxisAlignment.center,
         activeColor: Colors.grey[700],

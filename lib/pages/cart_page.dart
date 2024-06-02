f1f6bc2e3e76b284ecf0a22928bcbd2ecc176523
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-02 19:47:21 +0200

    Add drawer

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:coffee_app/components/cart_tile.dart';
 import 'package:coffee_app/models/coffee.dart';
+import 'package:coffee_app/navigators/app_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -14,51 +15,51 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Padding(
-      padding: const EdgeInsets.all(25.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("You Cart",
-              style: TextStyle(fontSize: 20, fontWeight:FontWeight.bold )),
-          SizedBox(
-            height: 25,
-          ),
-          Expanded(
-              child: ListView.builder(
-                  itemCount: 2,
-                  itemBuilder: (context, index) {
-                    Coffee coffee = Coffee(
-                        name: "Espresso",
-                        price: "3.50",
-                        imagePath: "lib/images/espresso.png");
-
-                    return CartTile(
-                      coffee: coffee,
-                      onPressed: (){},);
-                  })),
-
-                  GestureDetector(
-                    onTap: (){},
-                    child: Container(
-                      padding: EdgeInsets.all(25),
-                      width: double.infinity,
-                      child: Center(
-                        child: Text(
-                          "Pay Now",
-                          style: TextStyle(
-                            color: Colors.white
+        child: Scaffold(
+          body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("You Cart",
+                style: TextStyle(fontSize: 20, fontWeight:FontWeight.bold )),
+            const SizedBox(
+              height: 25,
+            ),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 2,
+                    itemBuilder: (context, index) {
+                      Coffee coffee = Coffee(
+                          name: "Espresso",
+                          price: "3.50",
+                          imagePath: "lib/images/espresso.png");
+        
+                      return CartTile(
+                        coffee: coffee,
+                        onPressed: (){},);
+                    })),
+        
+                    GestureDetector(
+                      onTap: (){},
+                      child: Container(
+                        padding: const EdgeInsets.all(25),
+                        width: double.infinity,
+                        child: const Center(
+                          child: Text(
+                            "Pay Now",
+                            style: TextStyle(
+                              color: Colors.white
+                            ),
                           ),
                         ),
+                        decoration: BoxDecoration(
+                          color: Colors.brown,
+                          borderRadius: BorderRadius.circular(12)
+                        ),
                       ),
-                      decoration: BoxDecoration(
-                        color: Colors.brown,
-                        borderRadius: BorderRadius.circular(12)
-                      ),
-                    ),
-                  )
-        ],
-      ),
-    ));
+                    )
+          ],
+        ),
+        drawer: const AppDrawer(),
+        ));
   }
 }

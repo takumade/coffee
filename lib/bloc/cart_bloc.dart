733cb9f66f351cd333f99f8ff08839faeb026d00
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-03 23:33:12 +0200

    Add cart events

--- lib/bloc/cart_bloc.dart
+++ lib/bloc/cart_bloc.dart
@@ -3,6 +3,21 @@
 
 import 'package:coffee_app/models/coffee.dart';
 
+sealed class CartEvent {
+  final Coffee coffee;
+
+  CartEvent({required this.coffee});
+}
+
+
+final class AddToCart extends CartEvent {
+  AddToCart({required super.coffee});
+}
+
+final class RemoveFromCart extends CartEvent {
+  RemoveFromCart({required super.coffee});
+}
+
 class Cart {
   List<Coffee> _cart = [];
 

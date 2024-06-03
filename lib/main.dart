c2ad89e63fd5302ada441a94cf0000ebafd6d2f7
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-03 23:52:12 +0200

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:coffee_app/bloc/cart_bloc.dart';
-import 'package:coffee_app/pages/home_page.dart';
 import 'package:coffee_app/pages/into_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';

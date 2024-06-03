e53310df531a901daf11b00d83ccea4b37470374
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-03 23:42:12 +0200

    Add more fields

--- lib/models/coffee.dart
+++ lib/models/coffee.dart
@@ -2,6 +2,8 @@ class Coffee {
   final String name;
   final String price;
   final String imagePath;
+  int? quantity;
+  String? size;
 
   Coffee({required this.name, required this.price, required this.imagePath});
 }

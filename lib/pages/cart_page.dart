f6769d6054de756a181f62ccbdb8c44c4f5e7c4c
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-04 01:20:26 +0200

    Add item removed dialog

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -19,67 +19,71 @@ class _CartPageState extends State<CartPage> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          backgroundColor: backgroundColor,
-          body: BlocListener<CartBloc, CartList>(
-            listener: (context, state) => {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text("Removed successfully!"))
-              )
-            }
-          ,
-            child: Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Your Cart",
-                    style: TextStyle(fontSize: 20, fontWeight:FontWeight.bold )),
-                const SizedBox(
-                  height: 25,
-                ),
-                Expanded(
-                    child: BlocBuilder<CartBloc, CartList>(
-                      builder: (context, state) => ListView.builder(
-                          itemCount: state.cart.length,
-                          itemBuilder: (context, index) {
-                            Coffee coffee = state.cart[index];
-                        
-                            return CartTile(
-                              coffee: coffee,
-                              onPressed: (){
-                                context.read<CartBloc>().add(
-                                  RemoveFromCart(coffee: coffee)
-                                );
-                              },);
-                          }),
-                    )),
-                      
-                        GestureDetector(
-                          onTap: (){},
-                          child: Container(
-                            padding: const EdgeInsets.all(25),
-                            width: double.infinity,
-                            child: const Center(
-                              child: Text(
-                                "Pay Now",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16
-                                ),
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.brown,
-                              borderRadius: BorderRadius.circular(12)
-                            ),
-                          ),
-                        )
-              ],
-                      ),
+      backgroundColor: backgroundColor,
+      body: BlocListener<CartBloc, CartList>(
+        listener: (context, state) => {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text(
+                "Successfully Removed!",
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.brown,
             ),
+          )
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Your Cart",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              const SizedBox(
+                height: 25,
+              ),
+              Expanded(
+                  child: BlocBuilder<CartBloc, CartList>(
+                builder: (context, state) => ListView.builder(
+                    itemCount: state.cart.length,
+                    itemBuilder: (context, index) {
+                      Coffee coffee = state.cart[index];
+
+                      return CartTile(
+                        coffee: coffee,
+                        onPressed: () {
+                          context
+                              .read<CartBloc>()
+                              .add(RemoveFromCart(coffee: coffee));
+                        },
+                      );
+                    }),
+              )),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  padding: const EdgeInsets.all(25),
+                  width: double.infinity,
+                  child: const Center(
+                    child: Text(
+                      "Pay Now",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.brown,
+                      borderRadius: BorderRadius.circular(12)),
+                ),
+              )
+            ],
           ),
-        drawer: const AppDrawer(),
-        ));
+        ),
+      ),
+      drawer: const AppDrawer(),
+    ));
   }
 }

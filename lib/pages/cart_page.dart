422a73af83b29acdaa00baf761180782a19274a0
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-04 01:12:48 +0200

    Add removed from cart listener

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,9 +1,11 @@
+import 'package:coffee_app/bloc/cart_bloc.dart';
 import 'package:coffee_app/components/cart_tile.dart';
 import 'package:coffee_app/const.dart';
 import 'package:coffee_app/models/coffee.dart';
 import 'package:coffee_app/navigators/app_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -18,53 +20,64 @@ class _CartPageState extends State<CartPage> {
     return SafeArea(
         child: Scaffold(
           backgroundColor: backgroundColor,
-          body: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text("Your Cart",
-                  style: TextStyle(fontSize: 20, fontWeight:FontWeight.bold )),
-              const SizedBox(
-                height: 25,
-              ),
-              Expanded(
-                  child: ListView.builder(
-                      itemCount: 2,
-                      itemBuilder: (context, index) {
-                        Coffee coffee = Coffee(
-                            name: "Espresso",
-                            price: "3.50",
-                            imagePath: "lib/images/espresso.png");
-                    
-                        return CartTile(
-                          coffee: coffee,
-                          onPressed: (){},);
-                      })),
-                    
-                      GestureDetector(
-                        onTap: (){},
-                        child: Container(
-                          padding: const EdgeInsets.all(25),
-                          width: double.infinity,
-                          child: const Center(
-                            child: Text(
-                              "Pay Now",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 16
+          body: BlocListener<CartBloc, CartList>(
+            listener: (context, state) => {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Removed successfully!"))
+              )
+            }
+          ,
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Your Cart",
+                    style: TextStyle(fontSize: 20, fontWeight:FontWeight.bold )),
+                const SizedBox(
+                  height: 25,
+                ),
+                Expanded(
+                    child: BlocBuilder<CartBloc, CartList>(
+                      builder: (context, state) => ListView.builder(
+                          itemCount: state.cart.length,
+                          itemBuilder: (context, index) {
+                            Coffee coffee = state.cart[index];
+                        
+                            return CartTile(
+                              coffee: coffee,
+                              onPressed: (){
+                                context.read<CartBloc>().add(
+                                  RemoveFromCart(coffee: coffee)
+                                );
+                              },);
+                          }),
+                    )),
+                      
+                        GestureDetector(
+                          onTap: (){},
+                          child: Container(
+                            padding: const EdgeInsets.all(25),
+                            width: double.infinity,
+                            child: const Center(
+                              child: Text(
+                                "Pay Now",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16
+                                ),
                               ),
                             ),
+                            decoration: BoxDecoration(
+                              color: Colors.brown,
+                              borderRadius: BorderRadius.circular(12)
+                            ),
                           ),
-                          decoration: BoxDecoration(
-                            color: Colors.brown,
-                            borderRadius: BorderRadius.circular(12)
-                          ),
-                        ),
-                      )
-            ],
-                    ),
+                        )
+              ],
+                      ),
+            ),
           ),
         drawer: const AppDrawer(),
         ));

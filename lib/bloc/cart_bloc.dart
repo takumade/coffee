b6aea5bd2796f87b890a2a8cd690451f851e752f
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-04 01:12:09 +0200

    Add cartlist calss

--- lib/bloc/cart_bloc.dart
+++ lib/bloc/cart_bloc.dart
@@ -19,21 +19,53 @@ final class RemoveFromCart extends CartEvent {
   RemoveFromCart({required super.coffee});
 }
 
-class CartBloc extends Bloc<CartEvent, List<Coffee>> {
 
-  CartBloc(): super([]) {
+class CartList {
+  List<Coffee> cart;
+
+  CartList({required this.cart});
+
+  List<Coffee> get userCart => cart;
+  
+  set cartList (List<Coffee> list){
+    cart = list;
+  }
+
+  List<Coffee> addItem(Coffee item){
+    cart.add(item);
+    return cart;
+  }
+
+  List<Coffee> removeItem(Coffee item){
+    cart.remove(item);
+    return cart;
+  }
+
+  CartList copyWith(List<Coffee> newCart){
+    return CartList(cart: newCart);
+  }
+
+  
+}
+
+class CartBloc extends Bloc<CartEvent, CartList> {
+
+  CartBloc(): super(CartList(cart: [])) {
     on<AddToCart>((event, emit) {
 
-      state.add(event.coffee);
-      emit(state);
+      var res = state;
+      res.addItem(event.coffee);
+      emit(state.copyWith(res.userCart));
     });
 
     on<RemoveFromCart>((event, emit){
-      state.remove(event.coffee);
-
-      emit(state);
+      print("Moo");
+      var res = state;
+      res.removeItem(event.coffee);
+      emit(state.copyWith(res.userCart));
     });
   }
+
 }
 
 

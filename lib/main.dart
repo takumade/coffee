69042d10f4d9cd1a28aa2de6125e277ea2df14eb
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-06-03 23:51:57 +0200

    Provide bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:coffee_app/bloc/cart_bloc.dart';
 import 'package:coffee_app/pages/home_page.dart';
 import 'package:coffee_app/pages/into_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +15,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiBlocProvider(providers: [
+      BlocProvider<CartBloc>(create: (BuildContext context) => CartBloc())
+    ], child: const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: IntroPage(),
-    );
+    ));
   }
 }
